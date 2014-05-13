110dd6c57b7d080fd65727ee516d98b517ab31da
Novozymes <novozymes@web7.it-kartellet.dk>
2014-05-13 02:34:55 +0200

    initial commit

---
+++ lang/en/theme_novo.php
@@ -0,0 +1,64 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'theme_clean', language 'en'
+ *
+ * @package   theme_novo
+ * @copyright 2013 Moodle, moodle.org
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['choosereadme'] = '
+<div class="clearfix">
+<div class="well">
+<h2>Novo</h2>
+<p><img class=img-polaroid src="novo/pix/screenshot.jpg" /></p>
+</div>
+<div class="well">
+<h3>About</h3>
+<p>Novo is a modified Moodle bootstrap theme which inherits styles and renderers from its parent theme.</p>
+<h3>Parents</h3>
+<p>This theme is based upon the Bootstrap theme, which was created for Moodle 2.5, with the help of:<br>
+Adrian Bordinc.</p>
+<h3>Theme Credits</h3>
+<p>Author: Adrian Bordinc<br>
+Contact: adrian@it-kartellet.dk<br>
+Website: <a href="http://www.it-kartellet.dk">www.it-kartellet.dk</a>
+</p>
+</div></div>';
+
+$string['configtitle'] = 'Novo';
+
+$string['customcss'] = 'Custom CSS';
+$string['customcssdesc'] = 'Whatever CSS rules you add to this textarea will be reflected in every page, making for easier customization of this theme.';
+
+$string['footnote'] = 'Footnote';
+$string['footnotedesc'] = 'Whatever you add to this textarea will be displayed in the footer throughout your Moodle site.';
+
+$string['invert'] = 'Invert navbar';
+$string['invertdesc'] = 'Swaps text and background for the navbar at the top of the page between black and white.';
+
+$string['logo'] = 'Logo';
+$string['logodesc'] = 'Please upload your custom logo here if you want to add it to the header.<br>
+If the height of your logo is more than 75px add the following CSS rule to the Custom CSS box below.<br>
+a.logo {height: 100px;} or whatever height in pixels the logo is.';
+
+$string['pluginname'] = 'Novo';
+
+$string['region-side-post'] = 'Right';
+$string['region-side-pre'] = 'Left';
+
